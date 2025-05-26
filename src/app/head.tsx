d13035a9457c58aa9bb57c6332b42bdcf00ca9d5
Sebastian Shan Reinaldo <sebastianshanreinaldo@gmail.com>
2025-05-26 11:58:08 +0700

    feat: added google site verification

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -6,7 +6,7 @@ export default function Head() {
         name="viewport"
         content="width=device-width, height=device-height"
       />
-      <title>Your App</title>
+      <meta name="google-site-verification" content="rz8NsHthyi-yFGVOi1zeyeG8wLfUTlr2-bAhpIqqhjk" />
     </>
   );
 }

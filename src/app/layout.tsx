6bfcff32f26701e0a66d4dcf73ac2d736c03874d
Sebastian Shan Reinaldo <sebastianshanreinaldo@gmail.com>
2025-05-26 12:04:36 +0700

    feat: migrated Head to Metadata api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
 import type { Metadata } from "next";
 import { SmoothCursor } from "@/components/ui/smooth-cursor";
 import "./globals.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Shanieulle - Cos Club ID",
   description:
     "The best time to be awake is when the world sleeps, for in those quiet moments, dreams are built and possibilities come alive.",
+  other: {
+    "google-site-verification": "rz8NsHthyi-yFGVOi1zeyeG8wLfUTlr2-bAhpIqqhjk",
+  },
 };
 
 export default function RootLayout({
